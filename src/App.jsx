0ece5229a9269162e096729c72e4b5c2480893cc
baselkurian <baselkurian@gmail.com>
2024-03-11 21:47:17 -0700

    navbar changes

--- src/App.jsx
+++ src/App.jsx
@@ -57,53 +57,22 @@ function App() {
   return (
     <>
       <div className="navbar bg-base-100 w-full">
+        {/* Left section */}
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">Dwebsite</a>
         </div>
 
-        {/* Search Bar */}
+        {/* Center section with a larger search bar */}
         <div className="flex-none">
-          <div className="flex-1 px-5">
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="Search"
-                className="input input-bordered w-full max-w-xs"
-              />
-            </div>
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-[500px]"
+            />
           </div>
         </div>
-
-        {/* Right section for avatar and dropdown */}
-        <div className="flex-1 justify-end flex">
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <div className="flex-1"></div>
       </div>
 
       {/* Shop Section */}
